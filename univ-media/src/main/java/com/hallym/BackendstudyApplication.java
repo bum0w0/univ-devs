fc8915da8a0458fcf05b91d1baebbee00c933121
bum0w0 <kgy5988@naver.com>
2025-05-09 19:17:06 +0900

    [Chore] 모듈 초기화용 기본 파일 추가

---
+++ univ-media/src/main/java/com/hallym/BackendstudyApplication.java
@@ -0,0 +1,11 @@
+package com.hallym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendstudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendstudyApplication.class, args);
+    }
+}
